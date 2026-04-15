4258ac2678738198cd3dc77473ca69a83532798e
Gamersooreal <lehanli06@gmail.com>
2026-04-15 23:56:03 +0530

    Added JAX-RS entry point and added the discovery endpoint to return API metadata

---
+++ CSA_20231796/CW_20231796/src/main/java/com/mycompany/cw_20231796/RestApplication.java
@@ -0,0 +1,8 @@
+package com.mycompany.cw_20231796;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class RestApplication extends Application {
+}
