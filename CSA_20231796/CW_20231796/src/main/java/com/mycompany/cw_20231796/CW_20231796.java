cdd2ba72984dba2c104a7055316adf54b4cf38ee
Gamersooreal <lehanli06@gmail.com>
2026-04-15 23:34:19 +0530

    setting up project with dependencies

---
+++ CSA_20231796/CW_20231796/src/main/java/com/mycompany/cw_20231796/CW_20231796.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cw_20231796;
+
+/**
+ *
+ * @author Dell
+ */
+public class CW_20231796 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
